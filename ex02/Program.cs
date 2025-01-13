8a19f8063caee1ea670b2e2f0309a4e3a885de21
POLLYANA SAYURI MATUZAKI DE BRITO <pollyana.smbrito@senacsp.edu.br>
2025-01-13 19:59:45 -0300

    first commit

---
+++ ex02/Program.cs
@@ -0,0 +1,6 @@
+﻿// Ex.02 - Escreva um programa que pergunte o nome do usuário,armazene esse nome numa variável name e apresente uma mensagem de boas vindas exibindo o nome usuário 
+
+Console.WriteLine("Informe o seu nome:");
+string name= Console.ReadLine();
+
+Console.WriteLine("seja bem-vindo(a)" + name);
---
+++ ex03/Program.cs
@@ -0,0 +1,16 @@
+﻿// ex _ 03 Esrever programa que solicite dois números inteiros para o usuário e exiba a soma dos números 
+
+Console.WriteLine("Digite o primeiro número:");
+int numero1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número:");
+int numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o terceiro número:");
+int numero3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o quarto número:");
+int numero4 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"A soma é: + {numero1 + numero2 + numero3 + numero4}"); 
+
